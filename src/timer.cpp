11ba156415e92e61e2fc97908b2e4777212e916f
opamp <opampg@gmail.com>
2011-11-18 21:49:03 +0900

    threadの原型みたいなのが完成

--- src/timer.cpp
+++ src/timer.cpp
@@ -2,8 +2,14 @@
 
 timer::timer()
 {
+    c = -1;
+    stop_f = false;
 }
 
 void timer::run(){
-
+    while(stop_f == false)
+    {
+        wait(1000);
+        emit oneSec(this->c);
+    }
 }
--- src/timer.hpp
+++ src/timer.hpp
@@ -5,11 +5,19 @@
 
 class timer : public QThread
 {
+Q_OBJECT
 public:
     timer();
+    bool stop_f; // this thread stop when this flag is true.
 
 protected:
     void run();
+
+private:
+    int c;
+
+signals:
+    void oneSec(int);
 };
 
 #endif // TIMER_HPP
