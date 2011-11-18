4deaeea11c32bff725a75e8405c57576e04fd857
opamp <opampg@gmail.com>
2011-11-18 18:59:51 +0900

    countdown_settingの基本的なUIを設定

--- src/countdown_setting.cpp
+++ src/countdown_setting.cpp
@@ -1,2 +1,24 @@
 #include "countdown_setting.hpp"
 
+countdown_setting::countdown_setting(QWidget *parent):
+    QDialog(parent)
+{
+    h = m = s = as = 0;
+    hb = new QSpinBox();
+    mb = new QSpinBox();
+    sb = new QSpinBox();
+
+    hl = new QLabel(tr("h"));
+    ml = new QLabel(tr("m"));
+    sl = new QLabel(tr("s"));
+
+    QHBoxLayout *mainlayout = new QHBoxLayout();
+    mainlayout->addWidget(hb);
+    mainlayout->addWidget(hl);
+    mainlayout->addWidget(mb);
+    mainlayout->addWidget(ml);
+    mainlayout->addWidget(sb);
+    mainlayout->addWidget(sl);
+
+    setLayout(mainlayout);
+}
--- src/countdown_setting.hpp
+++ src/countdown_setting.hpp
@@ -2,4 +2,17 @@
 #define COUNTDOWN_SETTING_HPP
 #include <QtGui>
 
+class countdown_setting : public QDialog
+{
+Q_OBJECT
+public:
+    countdown_setting(QWidget* parent = 0);
+
+private:
+    int h,m,s;
+    int as;
+
+    QSpinBox *hb,*mb,*sb;
+    QLabel *hl,*ml,*sl;
+};
 #endif
