da2bb3d80881954ae727ca1e1b3198cc78c1076f
opamp <opampg@gmail.com>
2011-11-19 13:56:09 +0900

    version 2.0

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -4,7 +4,19 @@ mainwindow::mainwindow()
 {
     w = new mainwid();
 
+    a_about = new QAction(tr("&About"),this);
+    connect(a_about,SIGNAL(triggered()),this,SLOT(about()));
+
+    help_bar = menuBar()->addMenu(tr("&Help and Info"));
+    help_bar->addAction(a_about);
 
     setCentralWidget(w);
     setWindowTitle(tr("Gat2"));
+}
+
+void mainwindow::about(){
+    QMessageBox::about(this,tr("About Gat2"),
+            tr("<h2>Gat2</h2>"
+               "<p>Copyright (c) 2011 opamp"
+               "<p>version:: 2.0")); // version
 }
--- src/mainwindow.hpp
+++ src/mainwindow.hpp
@@ -6,11 +6,17 @@
 
 class mainwindow : public QMainWindow
 {
+    Q_OBJECT
 public:
     mainwindow();
 
 private:
     mainwid *w;
+
+    QMenu *help_bar;
+    QAction *a_about;
+private slots:
+    void about();
 };
 
 #endif // MAINWINDOW_HPP
