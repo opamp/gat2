e48b56f6e93d09d1c429b82749157b71acd15b30
opamp <opampg@gmail.com>
2011-12-30 14:27:25 +0900

    デフォルトのWindowサイズを変更

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,17 @@
 #include <QApplication>
+#include <QSize>
 #include "mainwindow.hpp"
 
+#define default_width_size 450
+#define default_height_size 250
 
 int main(int argc,char* argv[]){
     QApplication gat(argc,argv);
 	gat.setApplicationName("gat2");
 
+	QSize mainwindow_size(default_width_size,default_height_size);
     mainwindow* window = new mainwindow();
+	window->setMinimumSize(mainwindow_size);
     window->show();
 
     return gat.exec();
