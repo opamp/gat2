4769c2e3ab329e140f366a6c008e92de84fadc5c
opamp sando <opampg@gmail.com>
2012-07-23 01:06:02 +0900

    version 2.5.3

--- src/version.hpp
+++ src/version.hpp
@@ -2,9 +2,9 @@
 #define GAT_VERSION_HPP
 #include<QString>
 
-#define gat_version "2.5.3 dev"
+#define gat_version "2.5.3"
 
-const QString config_file_name = ".gat.xml";//設定ファイル名を指定。2.5.3から有効
+const QString config_file_name = ".gat.xml";
 const QString config_file_path = QString::fromStdString(QDir::homePath().toStdString() + "/" + config_file_name.toStdString());
 const QString template_xml = ":/xmls/resource/xmls/template.xml";
 
